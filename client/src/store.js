4ab33635e179852cf7043cac16fbbc9651b23ed0
Camilo Avelar <camilo.avelar@gmail.com>
2018-06-18 22:51:20 -0300

    DevTools Bug

--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,16 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+    devTools = a => a;
+}
+
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), devTools));
 
 export default store;
